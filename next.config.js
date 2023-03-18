a69dbe64d1fcd80d027e3216142aab219055c11f
Marcello Antunes <marcello@Marcellos-MacBook-Air.local>
2023-03-18 16:27:34 -0300

    fix: fix next.config file build error

--- next.config.js
+++ next.config.js
@@ -1,16 +1,11 @@
 /** @type {import('next').NextConfig} */
 const path = require('path');
 
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-const sassConfig = {
-  includePaths: [path.join(__dirname, 'styles')],
-}
 
 
 module.exports = {
-  nextConfig,
-  sassConfig
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 };
