47648fc04032e1cb87471690cc5f1cfeafb53612
Marcello Antunes <marcello@Marcellos-MacBook-Air.local>
2023-03-20 18:52:28 -0300

    feature: create PostsList input

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 type Post = {
   id: string;
   name: string;
@@ -11,16 +13,33 @@ type Props = {
 };
 
 const PostList: React.FC<Props> = ({ posts, selectedPost }) => {
+  const [searchText, setSearchText] = useState("");
+
+  const filteredPosts = posts.filter((post) =>
+    post.name.toLowerCase().includes(searchText.toLocaleLowerCase())
+  );
+
   return (
     <div>
-      <ul className="post-list">
-        {posts.map((post) => (
-          <li key={post.id}>
-            <h3>{post.name}</h3>
-            {selectedPost?.id === post.id && <p>Selected</p>}
-          </li>
-        ))}
-      </ul>
+      <input
+        type="text"
+        placeholder="Filter posts"
+        className="post-filter"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+      />
+      {filteredPosts.length === 0 ? (
+        <p>No posts found</p>
+      ) : (
+        <ul className="post-list">
+          {filteredPosts.map((post) => (
+            <li key={post.id}>
+              <h3>{post.name}</h3>
+              {selectedPost?.id === post.id && <p>Selected</p>}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
