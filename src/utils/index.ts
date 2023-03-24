e026950febc969fd8684eee502db9968606450d6
Marcello Antunes <marcello@Marcellos-MacBook-Air.local>
2023-03-23 23:38:00 -0300

    refactor: create formatDate function

---
+++ src/utils/index.ts
@@ -0,0 +1,11 @@
+export const formatDate = (date: Date) =>
+  date.toLocaleString("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    hour12: true,
+    timeZoneName: "short",
+  });
